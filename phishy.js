24c12ce520203d59fe144730054cd8a867a83c0c
anthonygedeon <anthony.gedeonn@gmail.com>
2020-11-10 21:37:30 -0600

    list is rendered to the terminal for the user to pick a phishing website

--- phishy.js
+++ phishy.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-"use strict";
+'use strict';
 
 const fs = require('fs');
 const http = require('http');
@@ -8,3 +8,29 @@ const ngrok = require('ngrok');
 const inquirer = require('inquirer');
 const { exec } = require('child_process');
 
+inquirer
+	.prompt([
+		{
+			type: 'list',
+			name: 'websiteOption',
+			message: 'Please choose a website',
+			choices: [
+				'Amazon',
+				'Bank Of America',
+				'Google',
+				'iCloud',
+				'Instagram',
+				'LinkedIn',
+				'PNC',
+				'Royal Credit Union',
+				'PayPal',
+			],
+		},
+	])
+	.then((answer) => {
+		const ans = answer['websiteOption'];
+
+		const website = ans.split(' ').join('').toLowerCase();
+
+		console.log(website);
+	});
