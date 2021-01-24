e1765e3dbcec725697ed0e312f0ddede5b3d9523
Anthony Gedeon <anthonygedeon@Admins-MacBook-Pro.local>
2021-01-24 01:16:02 -0600

    menu functionality

--- phishy.sh
+++ phishy.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
+export COLOR_RESET="\e[0m"
+
+# Text Colors
+export WHITE_COLOR="\e[97m"
+export BLUE_COLOR="\e[34m"
+
+# Background Colors
+export BRED_COLOR="\e[41m"
+
 function banner() {
     clear
-    printf "\e[0m\n\e[97m"
-    printf "\e[0m\n\e[97m                 .                  \e[97mTwitter | https://www.twitter.com/ant_devs"
-    printf '\e[0m\n\e[97m                ":"                         \e'
-    printf '\e[0m\n\e[97m              ___:____     |"\/"|           \e'
-    printf "\e[0m\n\e[97m            ,'        '.    \  /            \e" 
-    printf "\e[0m\n\e[97m            |  O        \___/  |            \e" 
-    printf "\e[0m\n\e[97m            ~^~^~^~^~^~^~^~^~^~^~^~^~       \e"     
-    printf "\e[0m\n\e[34m ██████  ██   ██ ██ ███████ ██   ██ ██    ██ \e"
-    printf "\e[0m\n\e[34m ██   ██ ██   ██ ██ ██      ██   ██  ██  ██  \e"
-    printf "\e[0m\n\e[34m ██████  ███████ ██ ███████ ███████   ████   \e"
-    printf "\e[0m\n\e[34m ██      ██   ██ ██      ██ ██   ██    ██    \e"
-    printf "\e[0m\n\e[34m ██      ██   ██ ██ ███████ ██   ██    ██    \e[0m\n"
-    printf "\e[0m\n\e[1;41;97m Advanced Phishing tool with 30+ Templates \e[0m\n"
-    printf "\e[0m\n\e[34m"   
+
+    printf "$COLOR_RESET\n$WHITE_COLOR                 .                  $WHITE_COLOR Twitter | https://www.twitter.com/ant_devs$COLOR_RESET"
+    printf "$COLOR_RESET\n$WHITE_COLOR                \":\"                       $COLOR_RESET"
+    printf "$COLOR_RESET\n$WHITE_COLOR              ___:____     |\"\/\"|         $COLOR_RESET"
+    printf "$COLOR_RESET\n$WHITE_COLOR            ,'        '.    \  /            $COLOR_RESET" 
+    printf "$COLOR_RESET\n$WHITE_COLOR            |  O        \___/  |            $COLOR_RESET" 
+    printf "$COLOR_RESET\n$WHITE_COLOR            ~^~^~^~^~^~^~^~^~^~^~^~^~       $COLOR_RESET"     
+    printf "$COLOR_RESET\n$BLUE_COLOR ██████  ██   ██ ██ ███████ ██   ██ ██    ██ $COLOR_RESET"
+    printf "$COLOR_RESET\n$BLUE_COLOR ██   ██ ██   ██ ██ ██      ██   ██  ██  ██  $COLOR_RESET"
+    printf "$COLOR_RESET\n$BLUE_COLOR ██████  ███████ ██ ███████ ███████   ████   $COLOR_RESET"
+    printf "$COLOR_RESET\n$BLUE_COLOR ██      ██   ██ ██      ██ ██   ██    ██    $COLOR_RESET"
+    printf "$COLOR_RESET\n$BLUE_COLOR ██      ██   ██ ██ ███████ ██   ██    ██    $COLOR_RESET\n"
+    printf "$COLOR_RESET\n$BRED_COLOR$WHITE_COLOR Advanced Phishing tool with 30+ Templates $COLOR_RESET\n"
+    printf "$COLOR_RESET\n"   
 }
 
 function menuOptions() {
-    printf "\e[0m\e[97m Select A Template \e[0m\n"
-    printf "\e[0m\n"
+    printf "$COLOR_RESET$WHITE_COLOR Select A Template $COLOR_RESET$WHITE_COLOR"
+    printf "\n$COLOR_RESET\n"
+    printf "$COLOR_RESET$WHITE_COLOR[30] Amazon$COLOR_RESET\t$WHITE_COLOR[20] PNC\n$COLOR_RESET"
+    #read -p $' \e[97m Select an option: ' menu_option
 }
 
 banner
