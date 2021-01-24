a0ce7e33a8fa8ee572c59faf935a3cfdbceb99c7
Anthony Gedeon <anthonygedeon@Admins-MacBook-Pro.local>
2021-01-24 00:12:31 -0600

    bash version

---
+++ phishy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function banner() {
+    clear
+    printf "\e[0m\n\e[97m"
+    printf "\e[0m\n\e[97m                 .                  \e[97mTwitter | https://www.twitter.com/ant_devs"
+    printf '\e[0m\n\e[97m                ":"                         \e'
+    printf '\e[0m\n\e[97m              ___:____     |"\/"|           \e'
+    printf "\e[0m\n\e[97m            ,'        '.    \  /            \e" 
+    printf "\e[0m\n\e[97m            |  O        \___/  |            \e" 
+    printf "\e[0m\n\e[97m            ~^~^~^~^~^~^~^~^~^~^~^~^~       \e"     
+    printf "\e[0m\n\e[34m ██████  ██   ██ ██ ███████ ██   ██ ██    ██ \e"
+    printf "\e[0m\n\e[34m ██   ██ ██   ██ ██ ██      ██   ██  ██  ██  \e"
+    printf "\e[0m\n\e[34m ██████  ███████ ██ ███████ ███████   ████   \e"
+    printf "\e[0m\n\e[34m ██      ██   ██ ██      ██ ██   ██    ██    \e"
+    printf "\e[0m\n\e[34m ██      ██   ██ ██ ███████ ██   ██    ██    \e[0m\n"
+    printf "\e[0m\n\e[1;41;97m Advanced Phishing tool with 30+ Templates \e[0m\n"
+    printf "\e[0m\n\e[34m"   
+}
+
+function menuOptions() {
+    printf "\e[0m\e[97m Select A Template \e[0m\n"
+    printf "\e[0m\n"
+}
+
+banner
+
+menuOptions
